20dbc1895f0b1fc3879aa32a24ca5e5e940938a2
JerryHao <jerryhao2001@gmail.com>
2018-09-20 11:13:17 +0800

    9/20/2018

--- tests/recursionExercise.py
+++ tests/recursionExercise.py
@@ -32,11 +32,19 @@ def power_recur(a,b):
 
 
 def fib(n):
+    '''
+    input:n(int)
+    output:nst term of the sequence
+    '''
     if n == 0 or n == 1:
         return 1
     else:
         return fib(n-1)+fib(n-2)        
 def fib_sum(n):
+    '''
+    input:n(int)
+    output:sum of fist nst terms
+    '''
     if n == 0:
         return 1
     else:
@@ -46,6 +54,11 @@ def fib_sum(n):
 t = turtle.Turtle()
 
 def fractal(n = (-300.00,-200.00),l = 270):
+    '''
+    input:n start point
+          l length
+    output:graph
+    '''
     t.speed(100)
     t.color('blue')
     if l > 5:
@@ -62,10 +75,23 @@ def fractal(n = (-300.00,-200.00),l = 270):
         t.lt(120)
         jelly(b,l/2)
         jelly(c,l/2)
-        
-     
+
 print(sum_recur(4))
 print(fac_recur(4))
 print(power_recur(3,2))
 print(fib(5),fib_sum(5))
 fractal()
+        
+class test():
+    def __init__(self,temp=0):
+        self.temp = temp
+    def test_sum_recur(n = 5):
+        ans = sum(range(1,n+1)
+        if ans == sum_recur(5):
+            pass
+        else:
+            self.temp +=1
+            
+    
+    
+
