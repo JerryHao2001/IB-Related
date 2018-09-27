7a1dcf39debc13886aaa35d6b77cc87bb7a42ac0
JerryHao <jerryhao2001@gmail.com>
2018-09-27 11:08:18 +0800

    9/27/2018

---
+++ tests/bubbleSort.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 27 10:58:56 2018
+
+@author: 11658
+"""
+
+def bubbleSort(n):
+    """
+    input: list n
+    output: sorted list n
+    """
+    for i in range(len(n)-1):
+        for j in range(len(n)-1):
+            if n[j] > n[j+1]:
+                n[j],n[j+1] = n[j+1],n[j]
+    return(n)
